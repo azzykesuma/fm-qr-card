e976be7e094d0a2b53ee56dacdd81d95d3817887
Azzzy <official.azzy@gmail.com>
2023-05-11 09:37:21 +0630

    finishing prooject

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <div class="qr_card__main">
+      <img src="./assets/images/image-qr-code.png" alt="qr-code">
+      <div class="qr_card__bottom">
+        <h2 role="heading" aria-level="2">Improve your front-end skills by building projects</h2>
+        <p role="contentinfo">Scan the QR code to visit Fronted Mentor and take your coding skills to the next level</p>
+      </div>
+    </div>
+  </main>
+</template>
